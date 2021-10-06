fe382d27eb388903d2f6295425dfc02eb975dfed
ILANALI <90960557+ILANALI@users.noreply.github.com>
2021-10-06 18:13:53 +0530

    Create Robo.py

---
+++ Robo.py
@@ -0,0 +1,38 @@
+import turtle
+
+#set up the screen
+
+wn = turtle.Screen()
+
+wn.title("snake game")
+
+wn.bgcolor()
+
+wn.setup(width=600, height=600)
+
+wn.tracer(0)
+
+#Snake Head
+
+head = turtle.Turtle()
+
+head.speed(100)
+
+head.shape("square")
+
+head.color("green")
+
+head.penup()
+
+head.goto(0, 0)
+
+head.direction = "stop"
+
+# Main game loop
+
+while True:
+
+    wn.update()
+
+turtle.mainloop()
+
